9b2e8b8826ac8975f5542381a3c98817a09c8ee8
Dez Chuang <dissaivent@gmail.com>
2019-12-02 21:03:26 +0800

    Modified the play board style

--- src/views/Battle/index.vue
+++ src/views/Battle/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="pg-battle">
-    <div class="pg-battle__board-container">
-      <table class="pg-battle__board">
-        <tr :key="i"
-            v-for="i in 14"
-            class="row">
-          <td :key="j"
-              v-for="j in 14"
-              class="column" />
+    <div class="pg-battle__game-region">
+      <div class="pg-battle__play-board">
+        <div class="row"
+             :key="i"
+             v-for="(row, i) in playData">
+          <div class="column"
+               :key="j"
+               v-for="(column, j) in row" />
+        </div>
+      </div>
+      <table class="pg-battle__raw-board">
+        <tr class="row"
+            :key="i"
+            v-for="i in 14">
+          <td class="column"
+              :key="j"
+              v-for="j in 14" />
         </tr>
       </table>
     </div>
@@ -16,7 +25,28 @@
 
 <script>
   export default {
-    name: 'Battle'
+    name: 'Battle',
+    data() {
+      return {
+        playData: [
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+      }
+    }
   }
 </script>
 
@@ -25,19 +55,32 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    min-height: 700px;
-    &__board-container {
+    height: 100vh;
+    &__game-region {
+      background: url('../../assets/images/wood.jpg');
       display: flex;
       justify-content: center;
       align-items: center;
       width: 640px;
-      min-height: 640px;
-      background: url('../../assets/images/wood.jpg');
+      height: 640px;
+      position: relative;
     }
-    &__board {
+    &__play-board {
+      position: absolute;
+      color: transparent;
+      display: flex;
       .column {
-        border: 2px solid black;
         padding: 18px;
+        border: 2px solid grey;
+        margin-left: -2px;
+        margin-bottom: -2px;
+      }
+    }
+    &__raw-board {
+      position: absolute;
+      .column {
+        padding: 18px;
+        border: 2px solid #050505;
       }
     }
   }
